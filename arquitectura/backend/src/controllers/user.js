9c2468940a66a58b82379d6bbbb2f204932d6d3d
woscoff <73312435+woscoff@users.noreply.github.com>
2023-04-27 19:49:59 -0300

    arquitectura

---
+++ arquitectura/backend/src/controllers/user.js
@@ -0,0 +1,13 @@
+import { findUsers } from "../services/UserService.js"
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await findUsers()
+        res.status(200).send(users)
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+}
+
