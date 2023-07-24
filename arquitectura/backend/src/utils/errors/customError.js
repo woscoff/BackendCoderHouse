183fba0f9d622d835930d585fdf01c6c8ddf4dfb
woscoff <73312435+woscoff@users.noreply.github.com>
2023-07-24 15:41:09 -0300

    .

--- arquitectura/backend/src/utils/errors/customError.js
+++ arquitectura/backend/src/utils/errors/customError.js
@@ -6,7 +6,6 @@ export default class CustomError extends Error {
         this.code = code;
     }
 
-    // This method generates a new instance when called
     static createError({ name = `Error`, cause, message, code = 4 }) {
         const error = new CustomError({ name, cause, message, code });
         console.log(`${error.name}: ${error.cause}`)
